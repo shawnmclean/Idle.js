0a8488751e769c3381df2e3cc44ba86c512ce7f7
Shawn Mclean <mixmasterxp@gmail.com>
2013-04-25 18:28:05 -0500

    added rake build file

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,4 +1,5 @@
-require 'bundler'
+require 'rubygems'
+require 'bundler/setup'
 
 Bundler.setup
 
@@ -6,6 +7,6 @@ require 'uglifier'
 
 desc "Compile"
 task :compile do
-  system("bundle exec coffee -c -o ./src/idle.coffee ./build/")
+  system("bundle exec coffee -c -o ./build ./src")
   File.open('./build/idle.min.js', 'w') { |f| f.write Uglifier.compile(File.read('./build/idle.js')) }
 end
